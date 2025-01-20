f88f7a703d7ecacd11d8cc836592fe5333094ec4
Stephan van Jaarsveld <3183109+svanjaarsveld@users.noreply.github.com>
2025-01-20 21:03:00 +0200

    Add files via upload

--- notes_to_cloud.py
+++ notes_to_cloud.py
@@ -6,15 +6,11 @@ Created on Fri Jan 10 08:54:37 2025
 """
 
 # Import libraries
-#import gspread
-#import pandas as pd
-#from google.oauth2.service_account import Credentials
 import cv2
-from  PIL import Image
 import streamlit as st
 import easyocr
 import numpy as np
-#from pathlib import Path
+from PIL import Image
 
 
 def recognize_text(img_path):
@@ -47,23 +43,33 @@ def main():
     
     #Add column headings
     if uploaded_file is not None:
-        #image = Image.open(uploaded_file)
         rawBytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         cvImg = cv2.imdecode(rawBytes,cv2.IMREAD_COLOR)
         
         col1, col2 = st.columns( [0.5, 0.5])
         with col1:
             st.markdown('<p style="text-align: center;">Original</p>',unsafe_allow_html=True)
-            st.image(cvImg,channels='BGR',use_column_width=True)  
+            st.image(cvImg,channels='BGR',use_container_width=True)  
     
         with col2:
             st.markdown('<p style="text-align: center;">Text detection output</p>',unsafe_allow_html=True)
            
+            # recognize text
+            result = recognize_text(cvImg)
             
-            
-                
-                  
+            for (bbox, text, prob) in result:
+                #print(f'Detected text: {text} (Probability: {prob:.2f})')
+                # get top-left and bottom-right bbox vertices
+                (top_left, top_right, bottom_right, bottom_left) = bbox
+                top_left = (int(top_left[0]), int(top_left[1]))
+                bottom_right = (int(bottom_right[0]), int(bottom_right[1]))
+
+                # create a rectangle for bbox display
+                cv2.rectangle(img=cvImg, pt1=top_left, pt2=bottom_right, color=(0, 255, 0), thickness=3)
+                              
             st.image(cvImg, width=600)
+            
+            
     
 if __name__ == "__main__":
     main()
